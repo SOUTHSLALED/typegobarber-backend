732d8b0a44806fb9cfcff38370e502c93f26daf4
Drew de Sousa <southernharpin@gmail.com>
2020-07-02 14:42:23 -0300

    Commit inicial JWT

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+//preocupação da rota: receber uma requisição e devolver uma resposta
+
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+appointmentsRouter.get('/', async (req, res) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
+
+  return res.json(appointments);
+});
+
+appointmentsRouter.post('/', async (req, res) => {
+  try {
+    const { provider_id, date } = req.body;
+
+    const parsedDate = parseISO(date);
+
+    const CreateAppointment = new CreateAppointmentService();
+
+    const appointment = await CreateAppointment.execute({
+      date: parsedDate,
+      provider_id,
+    });
+
+    return res.json(appointment);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRouter;
